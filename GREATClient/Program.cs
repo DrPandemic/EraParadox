ef974149fcddd7f09ba1305b1c29d1d4ef97157a
William Turner <willtur.will@gmail.com>
2013-03-11 19:49:30 -0400

    Initial commit

---
+++ GREATClient/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GREATClient
+{
+  static class Program
+  {
+    private static Game1 game;
+
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      game = new Game1();
+      game.Run();
+    }
+  }
+}
