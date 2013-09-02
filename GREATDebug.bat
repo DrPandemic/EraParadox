e4e309a5c5ad6fee55e1e5454d8e2390ae1810b6
JesseEmond <emond.jesse@gmail.com>
2013-09-02 15:58:36 -0400

    + Batch files to start the server and client with a delay

---
+++ GREATDebug.bat
@@ -0,0 +1,4 @@
+start GREATServer\bin\Debug\GREATServer.exe
+timeout 2
+cd GREATClient
+start bin\Debug\GREATClient.Windows.exe
---
+++ GREATRelease.bat
@@ -0,0 +1,4 @@
+start GREATServer\bin\Release\GREATServer.exe
+timeout 2
+cd GREATClient
+start bin\Release\GREATClient.Windows.exe
